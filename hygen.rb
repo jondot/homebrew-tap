2428d6a7d714da47dbbc88aaa23920f2581b1a37
Dotan Nahum <jondotan@gmail.com>
2018-03-02 21:44:47 +0000

    hygen: auto-release

--- hygen.rb
+++ hygen.rb
@@ -1,6 +1,6 @@
 
-VER = "1.4.26"
-SHA = "ceb744bd2f1529a3bcecf947612b32f632fadd8f196c9c53bac9e83b9be06547"
+VER = "1.4.27"
+SHA = "3dbe62f405432db1437d8eb436c59bb3afd77542519c77c33eb267ce3cae1085"
 
 class Hygen < Formula
   desc "The scalable code generator that saves you time."
