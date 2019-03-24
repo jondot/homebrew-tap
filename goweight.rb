680fcc78ba07883697eda86a08835ff7ac8c4633
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-24 10:29:04 +0200

    Brew formula update for goweight version v1.0.2

--- goweight.rb
+++ goweight.rb
@@ -1,9 +1,9 @@
 class Goweight < Formula
   desc "A tool to analyze Go binary and module weights"
   homepage "https://github.com/jondot/goweight"
-  url "https://github.com/jondot/goweight/releases/download/v1.0.1/goweight_1.0.1_Darwin_x86_64.tar.gz"
-  version "1.0.1"
-  sha256 "ee6931f199c82022d67fffbf16192d91b2a8790bba8a7442b55509f05ea21305"
+  url "https://github.com/jondot/goweight/releases/download/v1.0.2/goweight_1.0.2_Darwin_x86_64.tar.gz"
+  version "1.0.2"
+  sha256 "3ce6acf01901f23606112bc070c0f325e92267bcfd9b15d13f729e4193fc2ee1"
 
   def install
     bin.install "goweight"
