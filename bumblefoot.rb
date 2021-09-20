346f50c5828d35659b73077209a228bbe0d402d8
Dotan J. Nahum <jondotan@gmail.com>
2021-09-20 17:48:53 +0300

    Update bumblefoot.rb

--- bumblefoot.rb
+++ bumblefoot.rb
@@ -1,9 +1,9 @@
 class Bumblefoot < Formula
   desc "A test formula"
   homepage "http://www.example.com"
-  url ""
-  version ""
-  sha256 ""
+  url "-------"
+  version "-------"
+  sha256 "--------"
 
   def install
     bin.install "bfoot"
