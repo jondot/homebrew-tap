10ac3abd30b9df446a7858dad8e64c3b3a6ff5b7
Dotan J. Nahum <jondotan@gmail.com>
2021-09-20 17:51:38 +0300

    updating formula for bumblefoot

--- bumblefoot.rb
+++ bumblefoot.rb
@@ -1,9 +1,9 @@
 class Bumblefoot < Formula
   desc "A test formula"
   homepage "http://www.example.com"
-  url "-------"
-  version "-------"
-  sha256 "--------"
+  url "https://github.com/SpectralOps/rust-ci-release-template/archive/v1.0.5.tar.gz"
+  version "1.0.5"
+  sha256 "501427f156bcd867007895bb2f6af12344f95bef524001e683b7a7d3c2631868"
 
   def install
     bin.install "bfoot"
