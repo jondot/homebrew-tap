c428d0c05c5ed71bbe4a749b20bfbb5bbba2d3ca
Dotan J. Nahum <jondotan@gmail.com>
2022-02-28 18:32:09 +0200

    updating formula for Hygen

--- hygen.rb
+++ hygen.rb
@@ -2,9 +2,9 @@
 class Hygen < Formula
   desc "The scalable code generator that saves you time."
   homepage "http://www.hygen.io"
-  url "https://github.com/jondot/hygen/releases/download/v6.1.4/hygen.macos.v6.1.4.tar.gz"
-  version "6.1.4"
-  sha256 "b6362b9e34ce71d27541a90547d86d1750ffa5bd2a616d488c4e6f5c8a42a9f2"
+  url "https://github.com/jondot/hygen/releases/download/v6.1.5/hygen.macos.v6.1.5.tar.gz"
+  version "6.1.5"
+  sha256 "862bf4d54238a1d51f4ce49044aea5fc3ee7f4f2066672f0c379cfc97cf85b00"
 
   def install
     bin.install "hygen"
