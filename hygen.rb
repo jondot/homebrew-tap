b05e4e6e068c0b6969d6f5957814a6bb10b2ffc7
Dotan J. Nahum <jondotan@gmail.com>
2022-02-28 18:12:23 +0200

    updating formula for Hygen

--- hygen.rb
+++ hygen.rb
@@ -5,7 +5,7 @@ SHA = "0803e9a19e421995e64a62228dc492e635b6d0aa572bed4d117b80c9810aa587"
 class Hygen < Formula
   desc "The scalable code generator that saves you time."
   homepage "http://www.hygen.io"
-  url "https://github.com/jondot/hygen/releases/download/v#{VER}/hygen.macos.v#{VER}.tar.gz"
+  url "https://github.com/jondot/hygen/releases/download/v6.1.4/hygen.macos.v6.1.4.tar.gz"
   version VER
   sha256 SHA
 
