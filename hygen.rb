4be02c2c06c6a7deb1c59b0dc4123802e7987a07
Dotan J. Nahum <jondotan@gmail.com>
2022-02-28 18:20:13 +0200

    migrate

--- hygen.rb
+++ hygen.rb
@@ -1,13 +1,10 @@
 
-VER = "6.1.0"
-SHA = "0803e9a19e421995e64a62228dc492e635b6d0aa572bed4d117b80c9810aa587"
-
 class Hygen < Formula
   desc "The scalable code generator that saves you time."
   homepage "http://www.hygen.io"
   url "https://github.com/jondot/hygen/releases/download/v6.1.4/hygen.macos.v6.1.4.tar.gz"
-  version VER
-  sha256 SHA
+  version "6.1.0"
+  sha256 "b6362b9e34ce71d27541a90547d86d1750ffa5bd2a616d488c4e6f5c8a42a9f2"
 
   def install
     bin.install "hygen"
