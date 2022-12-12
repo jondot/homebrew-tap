5438702b75c7d8a4faaa8560f37deebce1722d90
Dotan J. Nahum <jondotan@gmail.com>
2022-12-12 11:23:02 +0200

    rustwrap update: recon.rb

---
+++ recon.rb
@@ -0,0 +1,11 @@
+class Recon < Formula
+  desc "recon"
+  homepage "http://www.example.com"
+  url "https://github.com/rusty-ferris-club/recon/releases/download/v0.6.0/recon-x86_64-macos.tar.xz"
+  version "0.6.0"
+  sha256 "f2b8147f3154aac602149a27fe73f4b73ec09dd82225530c9a3eba6ad66b51ae"
+
+  def install
+    bin.install "recon"
+  end
+end
