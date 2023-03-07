080f64d06dcd54bcadc475b28eb77dfa5d99b97a
Dotan J. Nahum <jondotan@gmail.com>
2023-03-07 14:31:04 +0200

    rustwrap update: mm.rb

--- mm.rb
+++ mm.rb
@@ -1,9 +1,9 @@
 class Mm < Formula
   desc "mm - makeme: figures out how to start developing"
   homepage "http://github.com/jondot/makeme"
-  url "https://github.com/jondot/makeme/releases/download/v0.2.0/makeme-x86_64-macos.tar.xz"
-  version "0.2.0"
-  sha256 "a4472aa696ce28973ee77ae9ba6acde592420823ee210599e57c04e1f1869a84"
+  url "https://github.com/jondot/makeme/releases/download/v0.3.0/makeme-x86_64-macos.tar.xz"
+  version "0.3.0"
+  sha256 "2555b740c68da35356dbf7ae07542f4989e9ebde54239e11f1770cba8eec714d"
 
   def install
     bin.install "mm"
