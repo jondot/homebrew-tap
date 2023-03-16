795d5cbadaf473ecffffea0f26794040e85ed619
Dotan J. Nahum <jondotan@gmail.com>
2023-03-17 01:15:03 +0200

    rustwrap update: dg.rb

--- dg.rb
+++ dg.rb
@@ -1,9 +1,9 @@
 class Dg < Formula
   desc "desc"
   homepage "http://github.com/jondot/dg"
-  url "https://github.com/jondot/dg/releases/download/v1.0.1/dg-x86_64-macos.tar.xz"
-  version "1.0.1"
-  sha256 "6f8476dbdeaa5e1c27ba8b7be449de728bb5017f7a8f16deae1f0e0f970a80a4"
+  url "https://github.com/jondot/dg/releases/download/v1.0.3/dg-x86_64-macos.tar.xz"
+  version "1.0.3"
+  sha256 "37e0efc32973a12f4865e7884d733f428cce801567a45555bb8740d548082fce"
 
   def install
     bin.install "dg"
